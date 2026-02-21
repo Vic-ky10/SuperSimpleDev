6a1d220519f2affcd981f61c49f7d42f40f8eca3
vigneshwaran <vigneshvenketasan@gmail.com>
2026-02-21 11:53:50 +0530

    updated

--- React/vite-project/src/App.jsx
+++ React/vite-project/src/App.jsx
@@ -2,13 +2,17 @@ import { Routes, Route } from "react-router";
 import "./App.css";
 import HomePage from "./Pages/HomePage";
 import CheckoutPage from "./Pages/CheckoutPage";
+import OrderPage from "./Pages/OrderPage";
+import TrackingPage from "./Pages/TrackingPage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route index element={<HomePage />} />
-        <Route path="/checkout" element={<CheckoutPage />}/>
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/orders" element={<OrderPage />} />
+        <Route path="/tracking" element ={<TrackingPage />} />
       </Routes>
     </>
   );
